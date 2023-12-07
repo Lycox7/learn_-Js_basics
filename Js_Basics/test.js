b91afa7513066a1b73b465637718bc49ef076e54
Coder_Hahsam <153160450+Lycox7@users.noreply.github.com>
2023-12-07 05:45:22 +0000

    firstcode

---
+++ Js_Basics/test.js
@@ -0,0 +1,23 @@
+console.log("Asalam O Alikum");
+
+const CustomerId = 4569
+let EmailAddress = "Noor@google.com"
+var city         = "karachi"
+
+
+EmailAddress = "Saeed@hotmail.com"
+city         = "London"
+CustomerId   =  786
+
+
+
+/*
+    never use var because it didnt support the scope
+
+    const value does not change
+*/
+
+
+
+
+console.table([CustomerId, EmailAddress, city])
